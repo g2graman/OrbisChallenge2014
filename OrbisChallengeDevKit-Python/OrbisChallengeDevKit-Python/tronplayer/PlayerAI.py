ecbb84a3fe2de1d6fa7a21f767960fb6bcb36f4e
Francesco <fran.gramano@mail.utoronto.ca>
2014-09-21 02:58:33 -0400

    Adds functionality for retrieving potential moves.

--- OrbisChallengeDevKit-Python/OrbisChallengeDevKit-Python/tronplayer/PlayerAI.py
+++ OrbisChallengeDevKit-Python/OrbisChallengeDevKit-Python/tronplayer/PlayerAI.py
@@ -4,40 +4,96 @@ from tronclient.Client import *
 class PlayerAI():
 
 	def __init__(self):
+		self.num_moves = 0
 		return
+
 
 	def new_game(self, game_map, player_lightcycle, opponent_lightcycle):
+		self.num_moves = 0
+		#self.player_num = player_lightcycle['playerNumber']
+		#self.opponent_num = 3 - self.player_num
+
+		#msg = TronMessage()
+		#self.players = self.get_players(msg.players)
 		return
 
-	def get_move(self, game_map, player_lightcycle, opponent_lightcycle, moveNumber):
 
-		my_position = player_lightcycle['position']
+	def get_move(self, game_map, player_lightcycle, opponent_lightcycle, moveNumber):
+		self.num_moves += 1
+		self.my_pos = player_lightcycle['position']
+		self.opp_pos = opponent_lightcycle['position']
+		self.my_dir = player_lightcycle['direction']
+		self.opp_dir = opponent_lightcycle['direction']
+
+		my_position = self.my_pos
 		my_x = my_position[0]
 		my_y = my_position[1]
-		my_direction = player_lightcycle['direction']
-		
-		randMove = random.randint(0, 3)
-		if randMove == 0:
-			return PlayerActions.MOVE_LEFT
-		elif randMove == 1:
-			return PlayerActions.MOVE_RIGHT
-		elif randMove == 2:
-			return PlayerActions.MOVE_DOWN
-		else:
-			return PlayerActions.MOVE_UP
+		my_direction = self.my_dir
+
+		moves = self.moves(game_map, my_x, my_y)
+		randMove = random.randint(0, len(moves))
+		return moves[randMove - 1]
+
+
+	def heuristic(self):
+		return
+
+
+	def moves(self, game_map, my_x, my_y):
+		m = []
+		if(my_x >= 0 < len(game_map) - 1 and \
+		(game_map[my_x + 1][my_y] == EMPTY\
+		or game_map[my_x + 1][my_y] == POWERUP)):
+			m.append(PlayerActions.MOVE_RIGHT)
+		if(my_y >= 0 < len(game_map) - 1 and \
+		(game_map[my_x][my_y + 1] == EMPTY\
+		or game_map[my_x][my_y + 1] == POWERUP)):
+			m.append(PlayerActions.MOVE_DOWN)
+		if(my_x > 0 and (game_map[my_x - 1][my_y] == EMPTY\
+		 or game_map[my_x - 1][my_y] == POWERUP)):
+			m.append(PlayerActions.MOVE_LEFT)
+		if(my_y > 0 and (game_map[my_x][my_y - 1] == EMPTY\
+		or game_map[my_x][my_y - 1] == POWERUP)):
+			m.append(PlayerActions.MOVE_UP)
+
+		if(m == []):
+			m.append(PlayerActions.SAME_DIRECTION)
+			if(game_map[my_x + 1][my_y] == LIGHTCYCLE):
+				m.append(PlayerActions.MOVE_RIGHT)
+			elif(game_map[my_x - 1][my_y] == LIGHTCYCLE):
+				m.append(PlayerActions.MOVE_LEFT)
+			elif(game_map[my_x][my_y + 1] == LIGHTCYCLE):
+				m.append(PlayerActions.MOVE_DOWN)
+			elif(game_map[my_x][my_y - 1] == LIGHTCYCLE):
+				m.append(PlayerActions.MOVE_UP)
+		return m
+
+
+	def abeval(self, alpha, beta):
+		return
+
+
+	def wins(self):
+		opp_pos = self.players[self.opponent_num]['position']
+		my_pos = self.players[self.player_num]['position']
+		return
+
+
+	def is_terminal(self,):
+		return
 '''
 
-8888888 8888888888 8 888888888o.      ,o888888o.     b.             8 
-      8 8888       8 8888    `88.  . 8888     `88.   888o.          8 
-      8 8888       8 8888     `88 ,8 8888       `8b  Y88888o.       8 
-      8 8888       8 8888     ,88 88 8888        `8b .`Y888888o.    8 
-      8 8888       8 8888.   ,88' 88 8888         88 8o. `Y888888o. 8 
-      8 8888       8 888888888P'  88 8888         88 8`Y8o. `Y88888o8 
-      8 8888       8 8888`8b      88 8888        ,8P 8   `Y8o. `Y8888 
-      8 8888       8 8888 `8b.    `8 8888       ,8P  8      `Y8o. `Y8 
-      8 8888       8 8888   `8b.   ` 8888     ,88'   8         `Y8o.` 
+8888888 8888888888 8 888888888o.      ,o888888o.     b.             8
+      8 8888       8 8888    `88.  . 8888     `88.   888o.          8
+      8 8888       8 8888     `88 ,8 8888       `8b  Y88888o.       8
+      8 8888       8 8888     ,88 88 8888        `8b .`Y888888o.    8
+      8 8888       8 8888.   ,88' 88 8888         88 8o. `Y888888o. 8
+      8 8888       8 888888888P'  88 8888         88 8`Y8o. `Y88888o8
+      8 8888       8 8888`8b      88 8888        ,8P 8   `Y8o. `Y8888
+      8 8888       8 8888 `8b.    `8 8888       ,8P  8      `Y8o. `Y8
+      8 8888       8 8888   `8b.   ` 8888     ,88'   8         `Y8o.`
       8 8888       8 8888     `88.    `8888888P'     8            `Yo
-      
+
                                 Quick Guide
                 --------------------------------------------
                       Feel free to delete this comment.
@@ -74,9 +130,9 @@ class PlayerAI():
                                                 PlayerAction.ACTIVATE_POWERUP_MOVE_DOWN
                                                 PlayerAction.ACTIVATE_POWERUP_MOVE_LEFT
                                                 PlayerAction.ACTIVATE_POWERUP_MOVE_RIGHT
-                
+
         9. If you have any questions, contact challenge@orbis.com
 
         10. Good luck! Submissions are due Sunday, September 21 at noon. You can submit multiple times and your most recent submission will be the one graded.
- 
+
 '''
