95681a5370a92ae058d9b4759462ae1a6ab55151
Francesco <fran.gramano@mail.utoronto.ca>
2014-09-21 12:55:33 -0400

    Adds minimax depth-limited prototype (but still has a couple of errors after debugging for a bit)

--- OrbisChallengeDevKit-Python/OrbisChallengeDevKit-Python/tronplayer/PlayerAI.py
+++ OrbisChallengeDevKit-Python/OrbisChallengeDevKit-Python/tronplayer/PlayerAI.py
@@ -4,60 +4,68 @@ from tronclient.Client import *
 class PlayerAI():
 
 	def __init__(self):
-		self.num_moves = 0
 		return
 
 
 	def new_game(self, game_map, player_lightcycle, opponent_lightcycle):
-		self.num_moves = 0
-		#self.player_num = player_lightcycle['playerNumber']
-		#self.opponent_num = 3 - self.player_num
-
-		#msg = TronMessage()
-		#self.players = self.get_players(msg.players)
 		return
 
 
-	def get_move(self, game_map, player_lightcycle, opponent_lightcycle, moveNumber):
-		self.num_moves += 1
-		self.my_pos = player_lightcycle['position']
-		self.opp_pos = opponent_lightcycle['position']
-		self.my_dir = player_lightcycle['direction']
-		self.opp_dir = opponent_lightcycle['direction']
+	def get_move(self, game_map, player_lightcycle, opponent_lightcycle, moveNumber, m=[]):
+		m=self.mmax(game_map, player_lightcycle, opponent_lightcycle)
+		if(m == []):
+			my_x = player_lightcycle["position"][0]
+			my_y = player_lightcycle["position"][1]
 
-		my_position = self.my_pos
-		my_x = my_position[0]
-		my_y = my_position[1]
-		my_direction = self.my_dir
-
-		moves = self.moves(game_map, my_x, my_y)
-		randMove = random.randint(0, len(moves))
-		return moves[randMove - 1]
+			moves = self.moves(game_map, my_x, my_y, player_lightcycle)
+			randMove = random.randint(0, len(moves))
+			return moves[randMove - 1]
+		else:
+			return m.pop(0)
 
 
-	def heuristic(self):
-		return
+	def heuristic(self, g_map, p_cyc, opp_cyc):
+		term = self.is_terminal(g_map, p_cyc, opp_cyc)
+		if(term != []):
+			if(len(term) == 1):
+				if(term[0] != "opp"):
+					#I win
+					return 1000 #I win
+				else:
+					return -1000 #I lose
+			else:
+				return -10 #tie game
+		else:
+			pos_x = p_cyc["position"][0]
+			pos_y = p_cyc["position"][1]
+			if(g_map[pos_x][pos_y] == POWERUP):
+				return 5
+			return 0 #not terminal state
 
 
-	def moves(self, game_map, my_x, my_y):
+	def moves(self, game_map, my_x, my_y, p_cyc):
 		m = []
 		if(my_x >= 0 < len(game_map) - 1 and \
 		(game_map[my_x + 1][my_y] == EMPTY\
-		or game_map[my_x + 1][my_y] == POWERUP)):
+		or game_map[my_x + 1][my_y] == POWERUP or p_cyc["isInvincible"])):
 			m.append(PlayerActions.MOVE_RIGHT)
 		if(my_y >= 0 < len(game_map) - 1 and \
 		(game_map[my_x][my_y + 1] == EMPTY\
-		or game_map[my_x][my_y + 1] == POWERUP)):
+		or game_map[my_x][my_y + 1] == POWERUP or p_cyc["isInvincible"])):
 			m.append(PlayerActions.MOVE_DOWN)
 		if(my_x > 0 and (game_map[my_x - 1][my_y] == EMPTY\
-		 or game_map[my_x - 1][my_y] == POWERUP)):
+		 or game_map[my_x - 1][my_y] == POWERUP or p_cyc["isInvincible"])):
 			m.append(PlayerActions.MOVE_LEFT)
 		if(my_y > 0 and (game_map[my_x][my_y - 1] == EMPTY\
-		or game_map[my_x][my_y - 1] == POWERUP)):
+		or game_map[my_x][my_y - 1] == POWERUP or p_cyc["isInvincible"])):
 			m.append(PlayerActions.MOVE_UP)
 
 		if(m == []):
-			m.append(PlayerActions.SAME_DIRECTION)
+			#No safe moves
+
+			m.append(PlayerActions.SAME_DIRECTION) #Might as well keep going
+
+			#or settle for a draw
 			if(game_map[my_x + 1][my_y] == LIGHTCYCLE):
 				m.append(PlayerActions.MOVE_RIGHT)
 			elif(game_map[my_x - 1][my_y] == LIGHTCYCLE):
@@ -69,70 +77,126 @@ class PlayerAI():
 		return m
 
 
-	def abeval(self, alpha, beta):
-		return
+	def mmax(self, g_map, p_cyc, opp_cyc):
+		copy_map = g_map[:]
+		player_cyc = p_cyc.copy()
+		opponent_cyc = opp_cyc.copy()
+		m=[]
+		self._mmax(copy_map, player_cyc, opponent_cyc, 8, m)
+		return m
 
+	def _mmax(self, g_map, p_cyc, opp_cyc, depth=0, max_not_min=True, m=[]):
+		h = self.heuristic(g_map, p_cyc, opp_cyc)
+		if(depth==0 or h != 0):
+			return h
 
-	def wins(self):
-		opp_pos = self.players[self.opponent_num]['position']
-		my_pos = self.players[self.player_num]['position']
-		return
+		copy_map = g_map[:]
+		if(max_not_min):
+			player_cyc = p_cyc.copy()
+			best = float("-inf")
+			cyc_x = player_cyc["position"][0]
+			cyc_y = player_cyc["position"][1]
+			potential_moves = self.moves(copy_map, cyc_x, cyc_y, player_cyc)
+			for move in potential_moves:
+				if(move == PlayerActions.MOVE_UP \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and player_cyc["direction"] == Direction.UP)):
+					direc_x = 0
+					direc_y = -1
+				elif(move == PlayerActions.MOVE_DOWN \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and player_cyc["direction"] == Direction.DOWN)):
+					direc_x = 0
+					direc_y = 1
+				elif(move == PlayerActions.MOVE_LEFT \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and player_cyc["direction"] == Direction.LEFT)):
+					direc_x = -1
+					direc_y = 0
+				elif(move == PlayerActions.MOVE_RIGHT \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and player_cyc["direction"] == Direction.RIGHT)):
+					direc_x = 1
+					direc_y = 0
+				else:
+					direc_x = 0
+					direc_y = 0
 
+				pos_x = cyc_x + direc_x
+				pos_y = cyc_y + direc_y
+				copy_map[cyc_x][cyc_y] = TRAIL
+				player_cyc["position"]= (pos_x, pos_y)
+				copy_map[pos_x][pos_y] = LIGHTCYCLE
+				val = self._mmax(copy_map, player_cyc, opp_cyc, depth - 1, False, m)
+				if(val > best):
+					m.append(move)
+					best = max(val, best)
+			return best
+		else:
+			opponent_cyc = opp_cyc.copy()
+			best = float("inf")
+			cyc_x = opponent_cyc["position"][0]
+			cyc_y = opponent_cyc["position"][1]
+			for move in self.moves(copy_map, cyc_x, cyc_y, opponent_cyc):
+				if(move == PlayerActions.MOVE_UP \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and opponent_cyc["direction"] == Direction.UP)):
+					direc_x = 0
+					direc_y = -1
+				elif(move == PlayerActions.MOVE_DOWN \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and opponent_cyc["direction"] == Direction.DOWN)):
+					direc_x = 0\
+						or (move == PlayerActions.SAME_DIRECTION \
+							and opponent_cyc["direction"] == Direction.UP)
+					direc_y = 1
+				elif(move == PlayerActions.MOVE_LEFT \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and opponent_cyc["direction"] == Direction.LEFT)):
+					direc_x = -1
+					direc_y = 0
+				elif(move == PlayerActions.MOVE_RIGHT \
+					or (move == PlayerActions.SAME_DIRECTION \
+						and opponent_cyc["direction"] == Direction.RIGHT)):
+					direc_x = 1
+					direc_y = 0
+				else:
+					direc_x = 0
+					direc_y = 0
 
-	def is_terminal(self,):
-		return
-'''
+				pos_x = cyc_x + direc_x
+				pos_y = cyc_y + direc_y
+				copy_map[cyc_x][cyc_y] = TRAIL
+				opponent_cyc["position"] = (pos_x, pos_y)
+				copy_map[pos_x][pos_y] = LIGHTCYCLE
+				val = self._mmax(copy_map, p_cyc, opponent_cyc, depth - 1, True, m)
+				best = min(val, best)
+			return best
 
-8888888 8888888888 8 888888888o.      ,o888888o.     b.             8
-      8 8888       8 8888    `88.  . 8888     `88.   888o.          8
-      8 8888       8 8888     `88 ,8 8888       `8b  Y88888o.       8
-      8 8888       8 8888     ,88 88 8888        `8b .`Y888888o.    8
-      8 8888       8 8888.   ,88' 88 8888         88 8o. `Y888888o. 8
-      8 8888       8 888888888P'  88 8888         88 8`Y8o. `Y88888o8
-      8 8888       8 8888`8b      88 8888        ,8P 8   `Y8o. `Y8888
-      8 8888       8 8888 `8b.    `8 8888       ,8P  8      `Y8o. `Y8
-      8 8888       8 8888   `8b.   ` 8888     ,88'   8         `Y8o.`
-      8 8888       8 8888     `88.    `8888888P'     8            `Yo
+	def is_terminal(self, g_map, p_cyc, opp_cyc):
+		my_pos_x = p_cyc['position'][0]
+		my_pos_y = p_cyc['position'][1]
+		opp_pos_x = opp_cyc['position'][0]
+		opp_pos_y = opp_cyc['position'][1]
 
-                                Quick Guide
-                --------------------------------------------
-                      Feel free to delete this comment.
+		next_pos_me = g_map[my_pos_x][my_pos_y]
+		crash_me = next_pos_me == WALL or next_pos_me == TRAIL \
+			or (my_pos_x == opp_pos_x and my_pos_y == opp_pos_y)
 
-        1. THIS IS THE ONLY .PY OR .BAT FILE YOU SHOULD EDIT THAT CAME FROM THE ZIPPED STARTER KIT
+		next_pos_opp = g_map[opp_pos_x][opp_pos_y]
+		crash_opp = next_pos_opp == WALL or next_pos_opp == TRAIL \
+			or (my_pos_x == opp_pos_x and my_pos_y == opp_pos_y)
 
-        2. Any external files should be accessible from this directory
-
-        3. new_game is called once at the start of the game if you wish to initialize any values
-
-        4. get_move is called for each turn the game goes on
-
-        5. game_map is a 2-d array that contains values for every board position.
-                example call: game_map[2][3] == POWERUP would evaluate to True if there was a powerup at (2, 3)
-
-        6. player_lightcycle is your lightcycle and is what the turn you respond with will be applied to.
-                It is a dictionary with corresponding keys: "position", "direction", "hasPowerup", "isInvincible", "powerupType"
-                position is a 2-element int array representing the x, y value
-                direction is the direction you are travelling in. can be compared with Direction.DIR where DIR is one of UP, RIGHT, DOWN, or LEFT
-                hasPowerup is a boolean representing whether or not you have a powerup
-                isInvincible is a boolean representing whether or not you are invincible
-                powerupType is what, if any, powerup you have
-
-        7. opponent_lightcycle is your opponent's lightcycle. Same keys and values as player_lightcycle
-
-        8. You ultimately are required to return one of the following:
-                                                PlayerAction.SAME_DIRECTION
-                                                PlayerAction.MOVE_UP
-                                                PlayerAction.MOVE_DOWN
-                                                PlayerAction.MOVE_LEFT
-                                                PlayerAction.MOVE_RIGHT
-                                                PlayerAction.ACTIVATE_POWERUP
-                                                PlayerAction.ACTIVATE_POWERUP_MOVE_UP
-                                                PlayerAction.ACTIVATE_POWERUP_MOVE_DOWN
-                                                PlayerAction.ACTIVATE_POWERUP_MOVE_LEFT
-                                                PlayerAction.ACTIVATE_POWERUP_MOVE_RIGHT
-
-        9. If you have any questions, contact challenge@orbis.com
-
-        10. Good luck! Submissions are due Sunday, September 21 at noon. You can submit multiple times and your most recent submission will be the one graded.
-
-'''
+		if(crash_me):
+			if(crash_opp):
+				result = ["p", "opp"]
+			else:
+				result = ["opp"]
+		elif(crash_opp):
+			if(crash_me):
+				result = ["p", "opp"]
+			else:
+				result = ["p"]
+		else:
+			result = []
+		return result #result holds the winners in a list
